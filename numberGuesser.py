e2d6c56255d633b9627a9353a2a66e7706008347
Saaaannnns <github_z5174755@student.unsw.edu.au>
2018-08-10 15:57:12 +1000

    last change of bonus

--- numberGuesser.py
+++ numberGuesser.py
@@ -17,22 +17,40 @@ def handle_guess(guess, values):
     # This function should return a duplicate list of values
     # with the guessed value removed if it was present.
     pass
+    if int(guess) in values:
+        values.remove(int(guess))
+    return values
 
 def find_closest(guess, values):
     # This function should return the closest value
     # to the guessed value.
     pass
+    closest = values[0]
+    for i in range(1,len(values)):
+        if abs(int(guess) - closest) >= abs(int(guess) - values[i]):
+            closest = values[i]
+    return closest
 
 def run_game(values):
     # While there are values to be guessed and the user hasn't
     # quit (with 'q'), the game should wait for the user to input
     # their guess and then reveal whether the closest number is
     # lower or higher.
-    print(f'Numbers are between {MIN} and {MAX} inclusive. There are {len(values)} values left.')
-    guess = input('Guess: ')
+    print(f'Numbers are between {MIN} and {MAX} inclusive.')
     # Your code goes here.
-    
-    print('Thanks for playing! See you soon.')
+    while len(values) != 0:
+        guess = input(f'There are {len(values)} values left. Guess: ')        
+        if guess == 'q': break
+        closest = find_closest(guess, values)
+        if int(guess) == closest: print(f'You found {guess}! It was in the list.')
+        elif int(guess) < closest: print(f'The closest to {guess} is higher')
+        else: print(f'The closest to {guess} is lower')
+        values = handle_guess(guess, values)
+
+    if guess == 'q': print('Thanks for playing! See you soon.')
+    else:
+        print('Congratulations! You won!')
+        print('Thanks for playing! See you soon.')
 
 if __name__ == '__main__':
     values = sorted(random.sample(range(MIN, MAX+1), NUM_VALUES))
